a71af299ca2e069e3d5aad29735c738b1e2b171e
Marshall Lee <h.jermaine.lee@gmail.com>
2023-06-06 05:43:15 -0500

    second commit

--- modelos_DTs/Aritmeticos.py
+++
@@ -1,8 +0,0 @@
-def dt_esAritmetico(self, caracter):
-    operadores = [ "=", "-", "+", "/", "*", "%" ]
-    return self.perteneceLista(caracter, operadores)
-
-def dt_aritmeticos(self, lexema):
-
-    cont = self.marshall.contador
-    pos = self.marshall.uami.tabla.findSymbol(lexema)
